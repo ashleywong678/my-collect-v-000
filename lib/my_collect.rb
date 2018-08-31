4352516a5b3baceb5e55641d4dee4ccd526797d2
Ashley Wong <ashwong04@gmail.com>
2018-08-31 15:51:33 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def my_collect(arg)
   i = 0
   new_arg = []
@@ -7,3 +8,25 @@ def my_collect(arg)
   end
   new_arg
 end
+=======
+def my_collect(lang, names)
+  # i = 0
+  # new_lang = []
+  # while i < lang.length  
+  #   new_lang.push(yield lang[i].upcase)
+  #   i += 1
+  # end
+  # new_lang
+  
+  j = 0
+  new_names j= []
+  while j < names.length
+    yield new_names.push(names[j].split(" ").first)
+    j += 1
+  end
+  new_names
+  
+end
+
+
+>>>>>>> 6814b8553dbdec06f46c16258a513d1e1fe2ac6b
